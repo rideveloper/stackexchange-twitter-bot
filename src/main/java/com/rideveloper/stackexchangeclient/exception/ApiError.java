86393fbf0f2c9b68d2260137a0ba973017666e40
Ridwan <ridwan554@ymail.com>
2021-12-07 21:54:03 +0100

    added a general exception handler class

---
+++ src/main/java/com/rideveloper/stackexchangeclient/exception/ApiError.java
@@ -0,0 +1,42 @@
+package com.rideveloper.stackexchangeclient.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Ridwan Mustapha
+ */
+@Data
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+    private LocalDateTime timestamp;
+    private String debugMessage;
+
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+
+    ApiError(HttpStatus status) {
+        this();
+        this.status = status;
+    }
+
+    ApiError(HttpStatus status, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+
+    ApiError(HttpStatus status, String message, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = message;
+        this.debugMessage = ex.getLocalizedMessage();
+    }
+}
